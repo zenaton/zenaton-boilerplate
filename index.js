2a04aa77978aa63b9ffe37a55e9bb827eb54eecd
Antoine REYT <antoine.reyt@gmail.com>
2019-12-02 19:18:49 +0100

    fix express server

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.render('It works!'))
+  .get('/', (req, res) => res.send('It works!'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
