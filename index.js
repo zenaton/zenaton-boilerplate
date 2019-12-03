6dd34480af7347fd1fccc7c5ac587a282d7df5c9
Antoine REYT <antoine.reyt@gmail.com>
2019-12-03 10:50:47 +0100

    remove express server

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-
-express()
-  .get('/', (req, res) => res.send('It works!'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
