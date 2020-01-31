9375c13947df13a6ef4eb2dea3e45178fea88a0c
Gilles Barbier <gilles@zenaton.com>
2020-01-31 01:03:59 +0100

    cleaning docker

--- boot.js
+++ boot.js
@@ -1,12 +1,8 @@
 // load dependencies
-const { workflow, task } = require('zenaton')
+const { workflow, task } = require('zenaton');
 
-// load definitions
-const MyFirstWorkflow = require("./Workflows/MyFirstWorkflow")
-const MyFirstTask = require("./Tasks/MyFirstTask")
-const LogTask = require("./Tasks/LogTask")
+// define tasks
+task("myFirstTask", require("./tasks/myFirstTask"));
 
-// apply definitions
-workflow("MyFirstWorkflow", MyFirstWorkflow)
-task("MyFirstTask", MyFirstTask)
-task("LogTask", LogTask)
+// define workflows
+workflow("myFirstWorkflow", require("./workflows/myFirstWorkflow"));
